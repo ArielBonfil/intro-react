49d6510e9cf9a45abebb8c59e4f3aca452a84ddf
arielbonfil <ari@tobs.com.ar>
2020-06-05 19:05:54 -0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,11 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
+  addStundent(students){
+    this.setState({
+      student: this.state.students.concat(students),
+    });
+  }
 
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
